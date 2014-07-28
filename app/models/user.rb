dc835a922180451df0f9ac1d60a1eafba45e969c
AdrianMolina <adrian6_6_6@hotmail.com>
2014-07-28 18:08:43 -0400

    jefe de seguridad y jefe de brigada combobox

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def nombre_completo
+  	"#{nombre} #{apellido}"
+  end
 end

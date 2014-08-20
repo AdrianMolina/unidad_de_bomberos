55937881381c2ea620ccdc161449c5f3a497b212
douglascamposh <glassdc@gmail.com>
2014-08-20 15:59:24 -0400

    Nombre de conducotres en show de vehiculos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :emergencies
+  has_many :drivers, :dependent => :destroy, inverse_of: :user
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable

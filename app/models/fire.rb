00617dc86a0393d0767ab8dc87e33e8811af31c3
douglascamposh <glassdc@gmail.com>
2014-08-21 11:15:29 -0400

    agregado datos de los afectados en incendios

--- app/models/fire.rb
+++ app/models/fire.rb
@@ -1,5 +1,7 @@
 class Fire < ActiveRecord::Base
   belongs_to :emergency
   has_many :assist_fires, :dependent => :destroy, inverse_of: :fire
-  accepts_nested_attributes_for :assist_fires, :allow_destroy => true 
+  has_many :fire_affected_people, :dependent => :destroy, inverse_of: :fire
+  accepts_nested_attributes_for :assist_fires, :allow_destroy => true
+  accepts_nested_attributes_for :fire_affected_people, :allow_destroy => true
 end

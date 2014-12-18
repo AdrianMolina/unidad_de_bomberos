2372b9c8b52ad4c2ed1a6b0379a651a1aaed6d8d
douglascamposh <glassdc@gmail.com>
2014-12-18 04:05:57 +0000

    funciona perfecto sin errores kulas me la sudas arregla los exel pendejo

--- app/notifications/notification_sender.rb
+++ app/notifications/notification_sender.rb
@@ -5,12 +5,12 @@ class NotificationSender
     @emergency = emergency
     @gcm = GCM.new("AIzaSyAbQbjEui8JIt-IFh0SZyBhtaHLfX6ixKI")
     @registration_ids.each do |r|
-      send_message(r.registrationId, @emergency.tipo)
+      send_message(r.registrationId, @emergency)
     end
   end
-  def send_message(id_android, message)
+  def send_message(id_android, emergency)
     registration_ids= [id_android] # an array of one or more client registration IDs
-    options = {data: {mensaje: message}}
-    response = gcm.send(registration_ids, options)
+    options = {data: {mensaje: "Emergencia en progreso de tipo: "+emergency.tipo+", en la calle "+emergency.lugar}}
+    response = @gcm.send(registration_ids, options)
   end
 end

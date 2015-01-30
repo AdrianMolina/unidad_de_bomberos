bb1f8fa144149d42106dadb669869398d0142ae4
douglas campos <douglas14@hotmail.com>
2015-01-30 10:51:42 -0400

    solucionado bug de asignacion

--- app/models/android.rb
+++ app/models/android.rb
@@ -1,4 +1,12 @@
 class Android < ActiveRecord::Base
   belongs_to :emergency
   has_many :vehicles
+  def self.estaAsignado(android_id)
+  	android = Android.find(android_id)
+  	resultado = "no esta asignado a ningun vehiculo"
+  	if !android.vehicles.first.nil?
+  		resultado = android.vehicles.first.sigla
+  	end
+  	resultado
+  end
 end

492ea8e6c28f1a20e9d1cac1963e4bd6a65f12cb
douglas campos <douglas14@hotmail.com>
2015-01-30 11:12:48 -0400

    arreglado otro bug del show vehicle

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -6,4 +6,14 @@ class Vehicle < ActiveRecord::Base
 	validates :android_id, :uniqueness => { :message => "el dispositivo android ya esta asociado a otro vehiculo" }
 	validates :modelo, :numericality => { :only_integer => true, :message => "sólo se admite numeros" }, :allow_blank => true
   	accepts_nested_attributes_for :drivers, :allow_destroy => true
+  	def self.nombreDispositivo(vehicle)
+  		resultado = "Este vehiculo no tiene asignado un dispositivo android"
+  		if !vehicle.android.nil?
+  			resultado = vehicle.android_id
+  			if !vehicle.android.nombre.nil? or vehicle.android.nombre != ""
+  				resultado = vehicle.android.nombre
+  			end
+  		end
+  		resultado
+  	end
 end
